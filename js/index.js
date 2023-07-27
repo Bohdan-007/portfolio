ada207674217f169ebf645ba5c5b22fc633e3f3a
Bohdan-007 <elsetech133718@gmail.com>
2023-07-27 16:55:38 +0300

    added 'Portfolio' section and footer

--- js/index.js
+++ js/index.js
@@ -1,3 +1,79 @@
+// PORTFOLIO section
+class Work {
+  #photo;
+  #subtitle;
+  #title;
+  #description;
+  #btnText;
+  #workLink
+
+  constructor(photo, subtitle, title, description, btnText, workLink) {
+    this.#photo = photo;
+    this.#subtitle = subtitle;
+    this.#title = title;
+    this.#description = description;
+    this.#btnText = btnText;
+    this.#workLink = workLink;
+  };
+
+  get photo() { return this.#photo; };
+  get subtitle() { return this.#subtitle; };
+  get title() { return this.#title; };
+  get description() { return this.#description; };
+  get btnText() { return this.#btnText; };
+  get workLink() { return this.#workLink; };
+};
+
+
+const works = [
+  new Work('../images/portfolio-images/minimo-site.jpg', 'site', "A site about Someone's lifestyle",
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, id.Facere quidem quod error voluptate tempore corruptiaut eum deserunt.Quam ad quidem dolores praesentium ipsumcum, beatae sequi obcaecati ?',
+    'view project', 'https://bohdan-007.github.io/minimo/'),
+
+  new Work('../images/portfolio-images/tour-guide-site.jpg', 'site', 'tour guide site', 'some description...', 'view project', 'https://bohdan-007.github.io/tour-guide/'),
+
+  new Work('../images/portfolio-images/lms-site.jpg', 'web-app', 'LMS platform for any events', 'some description...', 'view project', 'https://bohdan-007.github.io/lms/'),
+
+  new Work('../images/portfolio-images/tic-tac-toe.jpg', 'web-game', 'TIC-TAC-TOE game for fun', 'some description...', 'view project', 'https://bohdan-007.github.io/tic-tac-toe/'),
+
+  new Work('../images/portfolio-images/animated.jpg', 'animation', 'animated site', 'some description...', 'view project', 'https://bohdan-007.github.io/animate-site/'),
+];
+
+
+const portfolioCol = $('#portfolioCol');
+
+works.forEach((work, index) => {
+  const row = $('<div class="row mb-5"></div>');
+
+  const colPhoto = $('<div class="col-12 col-lg-5 mb-4 mb-lg-0 mx-auto d-flex justify-content-center align-items-center portfolio__col-photo"></div>');
+  const img = $(`<img src="${work.photo}" alt="${work.photo}" class="w-100 rounded-3"/>`);
+  colPhoto.append(img);
+
+  const colDesc = $('<div class="col-12 col-lg-5 mb-5 mb-lg-0 mx-auto d-flex flex-column justify-content-center"></div>');
+  const subtitle = $(`<p class="m-0 text-white-50 text-uppercase">${work.subtitle}</p>`);
+  const title = $(`<h2 class="py-4 m-0 text-capitalize text-white">${work.title}</h2>`);
+  const description = $(`<p class="mb-5 text-white-50 fs-5 portfolio__desccription">${work.description}</p>`);
+  const button = $(`<a href="${work.workLink}" target="_blank" class="btn btn-primary px-5 py-2 border-0 text-uppercase align-self-start portfolio__button">${work.btnText}</a>`);
+  colDesc.append(subtitle);
+  colDesc.append(title);
+  colDesc.append(description);
+  colDesc.append(button);
+
+  if (index % 2 !== 0) {
+    colPhoto.addClass('order-1');
+    colDesc.addClass('order-0');
+  }
+  
+  row.append(colPhoto);
+  row.append(colDesc);
+  portfolioCol.append(row);
+});
+
+
+
+
+
+
 // EDUCATION section
 $(() => {
   $("#accordion").accordion({
